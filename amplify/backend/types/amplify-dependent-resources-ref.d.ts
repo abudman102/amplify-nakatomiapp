588655402cf3760620c9c251f55ffd7b7e7ffb25
Budman <ashleybudman@gmail.com>
2023-08-08 15:51:54 -0700

    routine push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,5 +15,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "fileupload": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- src/App.js
+++ src/App.js
@@ -8,32 +8,90 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
-import { API, Amplify, graphqlOperation } from 'aws-amplify';
+
+import { API, Amplify, graphqlOperation, Storage } from 'aws-amplify';
 import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
 
+import { listTodos } from './graphql/queries';
+
 import awsExports from "./aws-exports";
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
+  const [items, setItems] = useState([]);
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus ] = useState(false);
+
+  const uploadFile = async () => {
+    const file = fileData;
+
+    if (!file) {
+      alert('Please select a file before uploading');
+      return;
+    }
+
+    if(file.type !== 'text/csv') {
+      alert('Only CSV files are allowed');
+      return;
+    }
+
+    try {
+      const result = await Storage.put(file.name, file, {contentType: file.type});
+      setFileStatus(true);
+      console.log(21, result);
+    
+    } catch (error) {
+      console.error('File upload error',error);
+    }
+  
+  };
+
+
+   // const result = await Storage.put(fileData.name, fileData, {
+   //   contentType: fileData.type,
+   // });
+   // setFileStatus(true);
+   // console.log(21, result);
+  //};
+
   useEffect(() => {
     async function createTodoItem() {
       const todo = { name: "My third todo", description: "test!" };
 
+      /* create a todo */
       await API.graphql(graphqlOperation(createTodo, { input: todo }));
     }
 
-    createTodoItem();
+    async function listTodoItems() {
+      const todos = await API.graphql(graphqlOperation(listTodos));
+      console.log(30, todos.data.listTodos.items);
+      setItems(todos.data.listTodos.items);
+    
+    }
+    listTodoItems();
+    //createTodoItem();
+
   }, []);
 
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+    <div className="App">
+      <h1>Hello {user.username} </h1>
       <Button onClick={signOut}>Sign Out</Button>
-    </View>
+
+      {/*items.map((item, index) => (
+        <div key ={index}>
+          {item.name} - {item.description}
+        </div>
+      ))*/}
+      <div>
+        <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
+      </div>
+      <div>
+        <button onClick={uploadFile}>Upload file</button>
+      </div>
+      {fileStatus ? 'File uploaded successfully' : '' }
+    </div>
   );
 }
 
